29716635438fbf77e27af2b5884808834a390249
itservizi2 <itservizi.helpdesk@gmail.com>
2022-03-21 21:55:35 +0200

    add the code related to operations

---
+++ src/com/tekwill/topic3/ExecuteOperations.java
@@ -0,0 +1,12 @@
+package com.tekwill.topic3;
+
+public class ExecuteOperations {
+    public static void main(String[] args) {
+        Operations.printAritmeticOperations(5, 15);
+        Operations.printAritmeticOperations(7 , 8);
+        Operations.printLogicOperations(true, true);
+        Operations.useForAndWhile(38);
+
+
+    }
+}
